196523d364671becc995bc58b1ffb2f6ac84f98b
Kimmo Muli <kimmomuli@gmail.com>
2025-02-21 14:53:29 +0200

    Add Upstash Redis and rate limiting integration with tRPC

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,17 +1,33 @@
-import { initTRPC } from '@trpc/server';
+import { db } from '@/db';
+import { users } from '@/db/schema';
+import { auth } from '@clerk/nextjs/server';
+import { initTRPC, TRPCError } from '@trpc/server';
+import { eq } from 'drizzle-orm';
 import { cache } from 'react';
 import superjson from 'superjson';
+import { ratelimit } from '@/lib/retelimit';
+
 export const createTRPCContext = cache(async () => {
   /**
    * @see: https://trpc.io/docs/server/context
+   * 
+   * This function creates a context for each tRPC request.
+   * It acts like backend middleware by processing authentication
+   * and making user data available to all procedures.
    */
-  return { userId: 'user_123' };
+  const { userId } = await auth();
+  return {
+    clerkUserId: userId,
+  };
 });
+
+export type Context = Awaited<ReturnType<typeof createTRPCContext>>;
+
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
 // For instance, the use of a t variable
 // is common in i18n libraries.
-const t = initTRPC.create({
+const t = initTRPC.context<Context>().create({
   /**
    * @see https://trpc.io/docs/server/data-transformers
    */
@@ -21,3 +37,31 @@ const t = initTRPC.create({
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+
+
+export const protectedProcedure = t.procedure.use(async (opts) => {
+  const { ctx } = opts;
+
+  if (!ctx.clerkUserId) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' });
+  }
+
+  const [user] = await db.select().from(users).where(eq(users.clerkId, ctx.clerkUserId)).limit(1);
+
+  if (!user) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' });
+  }
+
+  const { success } = await ratelimit.limit(user.id)
+
+  if (!success) {
+    throw new TRPCError({ code: 'TOO_MANY_REQUESTS' });
+  } 
+
+  return opts.next({
+    ctx : {
+      ...ctx,
+      user,
+    }
+  });
+});
