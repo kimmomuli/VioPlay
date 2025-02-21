f4e2ed3d198ec1c6783280a8784a59dbd0d70948
Kimmo Muli <kimmomuli@gmail.com>
2025-02-21 13:36:47 +0200

    Add SuperJSON transformer to tRPC for better serialization

--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -2,21 +2,22 @@ import {
     defaultShouldDehydrateQuery,
     QueryClient,
   } from '@tanstack/react-query';
-//   import superjson from 'superjson';
-  export function makeQueryClient() {
+import superjson from 'superjson';
+
+export function makeQueryClient() {
     return new QueryClient({
       defaultOptions: {
         queries: {
           staleTime: 30 * 1000,
         },
         dehydrate: {
-          // serializeData: superjson.serialize,
+          serializeData: superjson.serialize,
           shouldDehydrateQuery: (query) =>
             defaultShouldDehydrateQuery(query) ||
             query.state.status === 'pending',
         },
         hydrate: {
-          // deserializeData: superjson.deserialize,
+          deserializeData: superjson.deserialize,
         },
       },
     });
