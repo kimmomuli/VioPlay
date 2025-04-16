b98b5589f9b0c28676917ca6d6f85270b18d4f31
Kimmo Muli <kimmomuli@gmail.com>
2025-04-16 14:46:51 +0300

    Video reactions ready

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -63,6 +63,22 @@ export const videoViews = pgTable("video_views", {
   }),
 ]);
 
+export const reactionType = pgEnum("reaction_type", ["like", "dislike"]);
+
+export const videoReactions = pgTable("video_reactions", {
+  userId: uuid("user_id").references(() => users.id, { onDelete: "cascade" }).notNull(),
+  videoId: uuid("video_id").references(() => videos.id, { onDelete: "cascade" }).notNull(),
+  type: reactionType("type").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+}, (t) => [
+  primaryKey({
+    name: "video_reactions_pk",
+    columns: [t.userId, t.videoId],
+  }),
+]);
+
+
 
 export const videoInsertSchema = createInsertSchema(videos);
 export const videoUpdateSchema = createUpdateSchema(videos);
@@ -72,3 +88,7 @@ export const videoSelectSchema = createSelectSchema(videos);
 export const videoViewSelectSchema = createSelectSchema(videoViews);
 export const videoViewInsertSchema = createInsertSchema(videoViews);
 export const videoViewUpdateSchema = createUpdateSchema(videoViews);
+
+export const videoReactionSelectSchema = createSelectSchema(videoReactions);
+export const videoReactionInsertSchema = createInsertSchema(videoReactions);
+export const videoReactionUpdateSchema = createUpdateSchema(videoReactions);
--- src/modules/videos/ui/components/video-reactions.tsx
+++ src/modules/videos/ui/components/video-reactions.tsx
@@ -2,20 +2,73 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { ThumbsDownIcon, ThumbsUpIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { VideoGetOneOutput } from "@/modules/videos/types";
+import { useClerk } from "@clerk/nextjs";
+import { trpc } from "@/trpc/client";
+import { toast } from "sonner";
 
-export const VideoReactions = () => {
-  const viewerReaction: "like" | "dislike" = "like";
+interface VideoReactionsProps {
+  videoId: string;
+  likes: number;
+  dislikes: number;
+  viewerReaction: VideoGetOneOutput['viewerReaction'];
+}
+
+
+export const VideoReactions = ({
+  videoId,
+  likes,
+  dislikes,
+  viewerReaction,
+}: VideoReactionsProps) => {
+  const clerk = useClerk();
+  const utils = trpc.useUtils();
+
+  const like = trpc.videoReactions.like.useMutation({
+    onSuccess: () => {
+      utils.videos.getOne.invalidate({ id: videoId });
+    },
+    onError: (error) => {
+      toast.error("Something went wrong");
+      if (error.data?.code === "UNAUTHORIZED") {
+        clerk.openSignIn();
+      }
+    },
+  });
+
+  const dislike = trpc.videoReactions.dislike.useMutation({ 
+    onSuccess: () => {
+      utils.videos.getOne.invalidate({ id: videoId });
+    },
+    onError: (error) => {
+      toast.error("Something went wrong");
+      if (error.data?.code === "UNAUTHORIZED") {
+        clerk.openSignIn();
+      }
+    },
+  });
+
 
   return (
     <div className="flex items-center flex-none">
-      <Button variant="secondary" className="rounded-l-md rounded-r-none gap-2 pr-4">
+      <Button
+        variant="secondary"
+        className="rounded-l-md rounded-r-none gap-2 pr-4"
+        onClick={() => like.mutate({ videoId })} 
+        disabled={like.isPending || dislike.isPending}  
+      >
            <ThumbsUpIcon className={cn("size-5", viewerReaction === "like" && "fill-black")} />
-           {1}
+           {likes}
       </Button>
-      <Separator orientation="vertical" className="h-10" />
-      <Button variant="secondary" className="rounded-l-none rounded-r-md pl-3">
-           <ThumbsDownIcon className={cn("size-5", viewerReaction !== "like" && "fill-black")} />
-           {1}
+      <Separator orientation="vertical" className="h-10 z-10" />
+      <Button
+        variant="secondary"
+        className="rounded-l-none rounded-r-md pl-3"
+        onClick={() => dislike.mutate({ videoId })}
+        disabled={like.isPending || dislike.isPending}
+      >
+           <ThumbsDownIcon className={cn("size-5", viewerReaction === "dislike" && "fill-black")} />
+           {dislikes}
       </Button>
     </div>
   );
